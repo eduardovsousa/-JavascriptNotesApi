3b31edf1224c8e2db5d83a2ce816f7fcca2a81de
eduardovsousa <eduardo.varela@outlook.com.br>
2022-04-06 01:50:41 -0300

    new deploy

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors());
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
